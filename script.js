5da95d8bd17eedbf011c03a35e3dc8ca7a53b0ae
Derek Tucker <30454354+dtuck20@users.noreply.github.com>
2018-01-20 19:20:25 -0700

    Updating Date

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-document.getElementsByClassName('date').textContent= (Date());
+window.onload = initDate();
+
+function initDate() {
+  var now = new Date();
+  now.getDate();
+document.getelementbyId("date").innerHTML= now;
+var now = new Date();
+now.setDate(now.getDate());
+document.getelementbyId("date").innerHTML = now.getDate();
+}
