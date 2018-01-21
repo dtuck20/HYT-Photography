a7b98769e0f60bc91a0194133baa841b34ed2b26
Derek Tucker <30454354+dtuck20@users.noreply.github.com>
2018-01-21 00:26:02 -0700

    CSS updates

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-window.onload = initDate();
 
-function initDate() {
-  var now = new Date();
-  now.getDate();
-document.getelementbyId("date").innerHTML= now;
-var now = new Date();
-now.setDate(now.getDate());
-document.getelementbyId("date").innerHTML = now.getDate();
-}
